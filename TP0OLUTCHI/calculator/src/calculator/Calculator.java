013c77d5ae383c86d653b08d81cf3a1171b48780
OLUTCHI <furcone.olutchi@epfedu.fr>
2023-09-25 19:55:21 +0200

    mon exo calculator tp0

---
+++ TP0OLUTCHI/calculator/src/calculator/Calculator.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/*TPA olutchi calculator
+ * MRG
+ * SEPTEMBRE 2023
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator:\n"
+                + "1) add\n"
+                + "2) substract\n"
+                + "3) multiply\n"
+                + "4) divide\n"
+                + "5) modulo");
+        
+        class Scanner sc = new Scanner(System.in);
+          System.out.print("Veuillez entrer un nombre entier : ");
+          int operateur =sc.nextInt();
+          class Scanner sc = new Scanner(System.in);
+          System.out.print("Veuillez entrer un nombre entier : ");
+          int operande1 =sc.nextInt();
+          class Scanner sc = new Scanner(System.in);
+          System.out.print("Veuillez entrer un nombre entier : ");
+          int operande2=sc.nextInt();
+          
+          
+
+    }
+
+}
