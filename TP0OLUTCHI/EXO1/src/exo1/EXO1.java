3c2f83a48f0cabaa79384f1f95f8cf46587681f7
OLUTCHI <furcone.olutchi@epfedu.fr>
2023-09-25 12:54:19 +0200

    mon exo 1 tp0

---
+++ TP0OLUTCHI/EXO1/src/exo1/EXO1.java
@@ -0,0 +1,31 @@
+/*
+ * TPO OLUTCHI EX0 1
+ * MRG
+ *Septembre 2023
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC
+ */
+public class EXO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Bonjour");
+        System.out.println("Bye Bye");
+
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+
+}
